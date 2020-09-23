2d7fdefa3c101f0c5fbf5008d6c5bb8dcaeeb9fb
unglock <106960347+unglock@users.noreply.github.com>
2020-09-23 18:02:34 +0000

    PNG

--- KafkaProducer/build.gradle.kts
+++ KafkaProducer/build.gradle.kts
@@ -17,4 +17,5 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.apache.kafka:kafka-streams:2.8.0")
     implementation("com.beust:klaxon:5.5")
-    implementation("org.jetbrains.kotlin:kotli
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
+    imp
