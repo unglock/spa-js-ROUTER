e380440b70b37b6305c1029e813350b50505df8d
unglock <106960347+unglock@users.noreply.github.com>
2020-11-12 11:20:10 +0000

    windfarm: decrement client count when unregistering

--- KafkaProducer/build.gradle.kts
+++ KafkaProducer/build.gradle.kts
@@ -21,4 +21,4 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api-kotlin:1.0.0")
     implementation("org.apache.logging.log4j:log4j-core:2.12.0")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.12.0")
-    impleme
+    implementation("io.ktor:ktor-server-netty:1
