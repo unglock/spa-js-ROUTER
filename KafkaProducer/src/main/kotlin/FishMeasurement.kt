737d6022f2dfd955daf0a38dea896d2781b170a9
unglock <106960347+unglock@users.noreply.github.com>
2021-03-08 09:21:50 +0000

    Create Solution.java

--- KafkaProducer/src/main/kotlin/FishMeasurement.kt
+++ KafkaProducer/src/main/kotlin/FishMeasurement.kt
@@ -5,4 +5,5 @@ data class FishMeasurement(
     val Fish_Id: String,
     val Species: String,
     val Length: Double,
-    val Height: Doub
+    val Height: Double,
+    val Timestamp: String? = S
