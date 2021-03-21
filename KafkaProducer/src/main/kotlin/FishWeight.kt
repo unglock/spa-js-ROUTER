c1ef6e73e27a46be5335b6e67d39d876159461c3
unglock <106960347+unglock@users.noreply.github.com>
2021-03-21 13:00:29 +0000

    image updated

--- KafkaProducer/src/main/kotlin/FishWeight.kt
+++ KafkaProducer/src/main/kotlin/FishWeight.kt
@@ -5,4 +5,4 @@ data class FishWeight(
     val Fish_Id: String,
     val Species: String,
     val Weight: Double,
-    val Timestamp: String? = SimpleDateFormat("yyyy-
+    val Timestamp: String? = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'.'SSS'Z'").format(Date
