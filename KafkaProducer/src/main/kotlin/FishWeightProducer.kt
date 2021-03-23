5b21c8f6e15e7f31456855768b477edb41d80a2b
unglock <106960347+unglock@users.noreply.github.com>
2021-03-23 15:16:37 +0000

    Add latest Ansible versions (2.5.10, 2.5.11, 2.6.7)

--- KafkaProducer/src/main/kotlin/FishWeightProducer.kt
+++ KafkaProducer/src/main/kotlin/FishWeightProducer.kt
@@ -1,3 +1,4 @@
 import com.beust.klaxon.Klaxon
 import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.Pro
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.logging.l
