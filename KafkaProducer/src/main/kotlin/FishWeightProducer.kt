fc56949ad881010a031e7449d8c77c714de4a73d
unglock <106960347+unglock@users.noreply.github.com>
2021-03-23 21:06:43 +0000

    dev-java/netty-codec: Cleaning old version(s)

--- KafkaProducer/src/main/kotlin/FishWeightProducer.kt
+++ KafkaProducer/src/main/kotlin/FishWeightProducer.kt
@@ -1,4 +1,6 @@
 import com.beust.klaxon.Klaxon
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.logging.l
+import org.apache.logging.log4j.kotlin.logger
+
+class FishWeight
