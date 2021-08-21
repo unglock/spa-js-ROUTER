b3e12389bc83952bde3c61cc44282e74ab70a95e
unglock <106960347+unglock@users.noreply.github.com>
2021-08-21 13:02:46 +0000

    [2.2.0rc1] Release preparation

--- KafkaStreams/src/main/kotlin/StreamProcessor.kt
+++ KafkaStreams/src/main/kotlin/StreamProcessor.kt
@@ -2,4 +2,5 @@ import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
-import org.apache.kafka.streams.kstrea
+import org.apache.kafka.streams.kstream.Consumed
+import org.apache.kaf
