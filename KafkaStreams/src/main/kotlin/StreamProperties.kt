91ce7b72dabaa1a7be707af9cb712272a78c67f1
unglock <106960347+unglock@users.noreply.github.com>
2021-09-21 10:00:21 +0000

    improve log

--- KafkaStreams/src/main/kotlin/StreamProperties.kt
+++ KafkaStreams/src/main/kotlin/StreamProperties.kt
@@ -6,4 +6,5 @@ class StreamProperties {
     fun configureProperties(): Properties {
 
         val settings = Properties()
-        settings.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "broker:
+        settings.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "broker:29092")
+        settings.set
