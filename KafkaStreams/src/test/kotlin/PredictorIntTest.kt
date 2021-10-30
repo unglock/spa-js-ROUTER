8bbc89af2c1eb56825f671c6e8ef9557b7d5912f
unglock <106960347+unglock@users.noreply.github.com>
2021-10-30 08:55:35 +0000

    Fixed C++ errors

--- KafkaStreams/src/test/kotlin/PredictorIntTest.kt
+++ KafkaStreams/src/test/kotlin/PredictorIntTest.kt
@@ -18,3 +18,5 @@ class PredictorIntTest: StringSpec() {
 
             val request = Fish("id", "salmon", 70.0, 18.0, "now")
             val response = predictor.requestWeight(request)
+
+            response.Length 
