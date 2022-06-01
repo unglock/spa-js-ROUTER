935be70872b6f38bf5356ad9180031ca7bc64003
unglock <106960347+unglock@users.noreply.github.com>
2022-06-01 14:51:37 +0000

    basic project setup

--- R/dataAnalysis/AfterAnalysis.R
+++ R/dataAnalysis/AfterAnalysis.R
@@ -14,4 +14,4 @@ library(ggplot2)
 ggplot() +
   geom_rect(aes(xmin = 33, xmax = 62, ymin = -Inf, ymax = Inf), fill = "#00AFBA", alpha = 0.7) +
   geom_rect(aes(xmin = 12, xmax = 62, ymin = -Inf, ymax = Inf), fill = "#00AFBA", alpha = 0.5) +
-  geom_r
+  geom_rect(aes(xmin = 62, xmax = 66, ym
