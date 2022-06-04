5f3627c4ce06c257f3d23ae42bd41f969bf7179b
unglock <106960347+unglock@users.noreply.github.com>
2022-06-04 17:52:25 +0000

    justdomains@mirror1.malwaredomains.com - Test

--- R/dataAnalysis/AfterAnalysis.R
+++ R/dataAnalysis/AfterAnalysis.R
@@ -15,4 +15,4 @@ ggplot() +
   geom_rect(aes(xmin = 33, xmax = 62, ymin = -Inf, ymax = Inf), fill = "#00AFBA", alpha = 0.7) +
   geom_rect(aes(xmin = 12, xmax = 62, ymin = -Inf, ymax = Inf), fill = "#00AFBA", alpha = 0.5) +
   geom_rect(aes(xmin = 62, xmax = 66, ymin = -Inf, ymax = Inf), fill = "#C5832E", alpha = 0.7) +
-  annotate("text", x = 64, y = 1.1, label = "Retr
+  annotate("text", x = 64, y = 1.1, label = "Retraining", color = "#173361") +
