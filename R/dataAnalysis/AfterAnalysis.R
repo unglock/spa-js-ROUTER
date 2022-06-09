8fa4a6b6176cf07d0658ba22b4ad44a3b2e50065
unglock <106960347+unglock@users.noreply.github.com>
2022-06-09 16:28:26 +0000

    tile

--- R/dataAnalysis/AfterAnalysis.R
+++ R/dataAnalysis/AfterAnalysis.R
@@ -16,4 +16,4 @@ ggplot() +
   geom_rect(aes(xmin = 12, xmax = 62, ymin = -Inf, ymax = Inf), fill = "#00AFBA", alpha = 0.5) +
   geom_rect(aes(xmin = 62, xmax = 66, ymin = -Inf, ymax = Inf), fill = "#C5832E", alpha = 0.7) +
   annotate("text", x = 64, y = 1.1, label = "Retraining", color = "#173361") +
-  annotate("text", x = 4
+  annotate("text", x = 45, y = 1.1, label = "Trigger"
