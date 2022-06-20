ffbccd0b36d7b318720472be68c662c211d66b55
unglock <106960347+unglock@users.noreply.github.com>
2022-06-20 07:04:18 +0000

    Create ccpp.yml

--- R/dataAnalysis/AfterAnalysis.R
+++ R/dataAnalysis/AfterAnalysis.R
@@ -26,4 +26,4 @@ ggplot() +
   theme(axis.title.x = element_text(colour = "#173361"),
         axis.title.y = element_text(colour = "#173361"),
         title = element_text(color = "#173361")) +
-  ann
+  annotate(geom = "text", 
