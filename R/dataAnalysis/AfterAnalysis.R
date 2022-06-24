884cfb0c75d26448cc5002fcceb3546d3c432c47
unglock <106960347+unglock@users.noreply.github.com>
2022-06-24 11:32:07 +0000

    updated tag

--- R/dataAnalysis/AfterAnalysis.R
+++ R/dataAnalysis/AfterAnalysis.R
@@ -34,4 +34,5 @@ ggplot() +
 
 
 ggplot() +
-  geom_rect(aes(xmin = 1, xmax = 49, ymin = -Inf, ymax = Inf), fill = "#00AFBA", alpha = 0.5) 
+  geom_rect(aes(xmin = 1, xmax = 49, ymin = -Inf, ymax = Inf), fill = "#00AFBA", alpha = 0.5) +
+  geom_rect(aes(xmin = 50, xm
