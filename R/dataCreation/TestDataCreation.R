0f3184ac161b2882b142e23cf53e1febc5aecf8d
unglock <106960347+unglock@users.noreply.github.com>
2022-08-01 12:49:21 +0000

    ssss

--- R/dataCreation/TestDataCreation.R
+++ R/dataCreation/TestDataCreation.R
@@ -12,4 +12,7 @@ height <- round(rnorm(60, 19, 1), 3)
 error <- round(rnorm(60, 0, 0.1), 3)
 weight <- 0.05 * length + 0.01 * height + error
 
-data1 <- data.frame(species = "salmon", id = seq(1, 60), length = length, heig
+data1 <- data.frame(species = "salmon", id = seq(1, 60), length = length, height = height, weight = weight)
+
+
+# Walley
