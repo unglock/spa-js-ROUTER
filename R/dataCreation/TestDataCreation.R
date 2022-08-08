1c8bcf9bfeab2af914774b151baa90745a2e73cf
unglock <106960347+unglock@users.noreply.github.com>
2022-08-08 19:09:54 +0000

    new scripts

--- R/dataCreation/TestDataCreation.R
+++ R/dataCreation/TestDataCreation.R
@@ -23,4 +23,4 @@ height <- round(rnorm(60, 17, 2), 3)
 error <- round(rnorm(60, 0, 0.1), 3)
 weight <- 0.02 * length + 0.04 * height + error
 
-data2 <- data.frame(species = "walleye", id = seq(6
+data2 <- data.frame(species = "walleye", id = seq(61, 120), length = length, height 
