55ef33958970715879b6c4b4bfaefc314052d45f
unglock <106960347+unglock@users.noreply.github.com>
2022-08-24 16:07:42 +0000

    Source

--- R/dataCreation/TestDataCreation.R
+++ R/dataCreation/TestDataCreation.R
@@ -34,4 +34,4 @@ height <- round(rnorm(60, 19, 1), 3)
 error <- round(rnorm(60, 0, 0.1), 3)
 weight <- 0.05 * length + 0.01 * height + error
 
-data3 <- data.frame(species = "salmon", id
+data3 <- data.frame(species = "salmon", id = seq(121, 180), length = length, 
