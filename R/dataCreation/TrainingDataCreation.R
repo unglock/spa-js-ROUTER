ebfd30e005a5c4c3228f0e2b8c718ef47a6688dc
unglock <106960347+unglock@users.noreply.github.com>
2022-09-17 09:55:43 +0000

    Remove deprecated stuff

--- R/dataCreation/TrainingDataCreation.R
+++ R/dataCreation/TrainingDataCreation.R
@@ -2,4 +2,6 @@ set.seed(200)
 
 length <- round(rnorm(100, 88, 5), 3)
 height <- round(rnorm(100, 19, 1), 3)
-error <- round(rnorm(100, 0, 0.1), 3
+error <- round(rnorm(100, 0, 0.1), 3)
+
+weight <- 0.05 * length +
