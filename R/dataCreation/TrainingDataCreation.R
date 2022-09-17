a8b89a3afaf49c1751e8ea40852db013ed99713b
unglock <106960347+unglock@users.noreply.github.com>
2022-09-17 14:16:43 +0000

    geolocation

--- R/dataCreation/TrainingDataCreation.R
+++ R/dataCreation/TrainingDataCreation.R
@@ -4,4 +4,6 @@ length <- round(rnorm(100, 88, 5), 3)
 height <- round(rnorm(100, 19, 1), 3)
 error <- round(rnorm(100, 0, 0.1), 3)
 
-weight <- 0.05 * length +
+weight <- 0.05 * length + 0.01 * height + error
+
+data <- 
