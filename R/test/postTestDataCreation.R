3e7b96abb6bf4d4ef51c66f940bfbff3428b69d4
unglock <106960347+unglock@users.noreply.github.com>
2022-10-29 20:48:54 +0000

    Add disqus

---
+++ R/test/postTestDataCreation.R
@@ -0,0 +1,11 @@
+
+library(mongolite)
+library(jsonlite)
+connection <- mongo(collection = "TrainingData",
+                    db = "Weight",
+                    url = "mongodb://user:password@localhost:27017")
+
+
+data <- connection$find()
+data <- data[0:75, ]
+write_json(data, path = "postTestData.json")
